3b1773e677ee75ace87b5034afdb0548fd398d3b
NaeemaBsharat <naeemabsharat1@gmail.com>
2023-10-07 21:12:05 +0300

    web code editor

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+
+function editor(){
+    const htmlcode=document.getElementById("html-code");
+    const csscode=document.getElementById("css-code");
+    const jscode=document.getElementById("javascript-code");
+    const output=document.getElementById("output").contentWindow.document;
+
+    document.body.onkeyup=function(){
+        output.open();
+        output.writeln(
+            htmlcode.value + 
+
+            "<style>"+csscode.value+"<style>" +
+
+            "<script>"+jscode.value +"</script>"
+        );
+
+
+        output.close();
+
+
+
+
+
+    }
+
+}
+editor();
